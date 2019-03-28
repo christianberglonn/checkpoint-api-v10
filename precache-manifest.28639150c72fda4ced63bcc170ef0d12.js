e1b92682c9ef50efcdbcbed97c89206a57a91365
slainedinvain <christianberglonn@gmail.com>
2019-03-28 10:35:05 +0100

    Updates

---
+++ precache-manifest.28639150c72fda4ced63bcc170ef0d12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a69da6b09e913fa1d96e",
+    "url": "/checkpoint-api-v10/static/js/runtime~main.41fee87f.js"
+  },
+  {
+    "revision": "c81744583950417fc9ab",
+    "url": "/checkpoint-api-v10/static/js/main.faeeed46.chunk.js"
+  },
+  {
+    "revision": "3b5aab7adbfd85dfd191",
+    "url": "/checkpoint-api-v10/static/js/2.b8364f69.chunk.js"
+  },
+  {
+    "revision": "c81744583950417fc9ab",
+    "url": "/checkpoint-api-v10/static/css/main.af51d05a.chunk.css"
+  },
+  {
+    "revision": "3b5aab7adbfd85dfd191",
+    "url": "/checkpoint-api-v10/static/css/2.ce6b8fba.chunk.css"
+  },
+  {
+    "revision": "80be3eb4e167d486b5ab7df37a712d45",
+    "url": "/checkpoint-api-v10/index.html"
+  }
+];
